c6ed49d0b05f93f06c7600f199f3b0dd9d9d6a44
kaneki-jvk913 <aboutrfass@gmail.com>
2025-03-02 19:27:18 +0000

    update configs

--- .config/github-scripts/refresh-local-repo.sh
+++ .config/github-scripts/refresh-local-repo.sh
@@ -5,5 +5,7 @@
 /home/ayoub/.config/github-scripts/scripts/2-copy-script-config.sh
 /home/ayoub/.config/github-scripts/scripts/3-copy-script-home-dots.sh
 
+rm -r /home/ayoub/.emacs.d/elpa/gnupg
+
 echo "Scripts executed successfully in order."
 
