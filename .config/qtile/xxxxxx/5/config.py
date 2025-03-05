f06e0b2d23e3079e427278a4ceeac21abbdfac0b
kaneki-jvk913 <aboutrfass@gmail.com>
2025-03-05 17:11:06 +0000

    update qtile

--- .config/qtile/xxxxxx/5/config.py
+++ .config/qtile/xxxxxx/5/config.py
@@ -1,7 +1,7 @@
 
 import os
 import subprocess
-import colors
+
 
 from libqtile import bar, layout, qtile, widget, hook, extension
 from libqtile.config import Click, Drag, Group, Key, Match, Screen, KeyChord
@@ -244,181 +244,132 @@ def autostart():
 
 
 
-
-
-colors = colors.DoomOne
-
-
-layout_theme = {"border_width": 2,
-                "margin": 8,
-                "border_focus": colors[8],
-                "border_normal": colors[0]
-                }
-
-
 screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.Image(
-                 filename = "~/.config/qtile/icons/logo.png",
-                 scale = "False",
-                 mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm)},
-                 ),
-        widget.Prompt(
-                 font = "Ubuntu Mono",
-                 fontsize=14,
-                 foreground = colors[1]
-        ),
-        widget.GroupBox(
-                 fontsize = 11,
-                 margin_y = 5,
-                 margin_x = 5,
-                 padding_y = 0,
-                 padding_x = 1,
-                 borderwidth = 3,
-                 active = colors[8],
-                 inactive = colors[1],
-                 rounded = False,
-                 highlight_color = colors[2],
-                 highlight_method = "line",
-                 this_current_screen_border = colors[7],
-                 this_screen_border = colors [4],
-                 other_current_screen_border = colors[7],
-                 other_screen_border = colors[4],
-                 ),
-        widget.TextBox(
-                 text = '|',
-                 font = "Ubuntu Mono",
-                 foreground = colors[1],
-                 padding = 2,
-                 fontsize = 14
-                 ),
-        widget.CurrentLayoutIcon(
-                 # custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
-                 foreground = colors[1],
-                 padding = 4,
-                 scale = 0.6
-                 ),
-        widget.CurrentLayout(
-                 foreground = colors[1],
-                 padding = 5
-                 ),
-        widget.TextBox(
-                 text = '|',
-                 font = "Ubuntu Mono",
-                 foreground = colors[1],
-                 padding = 2,
-                 fontsize = 14
-                 ),
-        widget.WindowName(
-                 foreground = colors[6],
-                 max_chars = 40
-                 ),
-        widget.GenPollText(
-                 update_interval = 300,
-                 func = lambda: subprocess.check_output("printf $(uname -r)", shell=True, text=True),
-                 foreground = colors[3],
-                 fmt = '❤  {}',
-                 decorations=[
-                     BorderDecoration(
-                         colour = colors[3],
-                         border_width = [0, 0, 2, 0],
-                     )
-                 ],
-                 ),
-        widget.Spacer(length = 8),
-        widget.CPU(
-                 format = '▓  Cpu: {load_percent}%',
-                 foreground = colors[4],
-                 decorations=[
-                     BorderDecoration(
-                         colour = colors[4],
-                         border_width = [0, 0, 2, 0],
-                     )
-                 ],
-                 ),
-        widget.Spacer(length = 8),
-        widget.Memory(
-                 foreground = colors[8],
-                 mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e htop')},
-                 format = '{MemUsed: .0f}{mm}',
-                 fmt = '🖥  Mem: {} used',
-                 decorations=[
-                     BorderDecoration(
-                         colour = colors[8],
-                         border_width = [0, 0, 2, 0],
-                     )
-                 ],
-                 ),
-        widget.Spacer(length = 8),
-        widget.DF(
-                 update_interval = 60,
-                 foreground = colors[5],
-                 mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e df')},
-                 partition = '/',
-                 #format = '[{p}] {uf}{m} ({r:.0f}%)',
-                 format = '{uf}{m} free',
-                 fmt = '🖴  Disk: {}',
-                 visible_on_warn = False,
-                 decorations=[
-                     BorderDecoration(
-                         colour = colors[5],
-                         border_width = [0, 0, 2, 0],
-                     )
-                 ],
-                 ),
-        widget.Spacer(length = 8),
-        widget.Volume(
-                 foreground = colors[7],
-                 fmt = '🕫  Vol: {}',
-                 decorations=[
-                     BorderDecoration(
-                         colour = colors[7],
-                         border_width = [0, 0, 2, 0],
-                     )
-                 ],
-                 ),
-        widget.Spacer(length = 8),
-        widget.KeyboardLayout(
-                 foreground = colors[4],
-                 fmt = '⌨  Kbd: {}',
-                 decorations=[
-                     BorderDecoration(
-                         colour = colors[4],
-                         border_width = [0, 0, 2, 0],
-                     )
-                 ],
-                 ),
-        widget.Spacer(length = 8),
-        widget.Clock(
-                 foreground = colors[8],
-                 format = "⏱  %a, %b %d - %H:%M",
-                 decorations=[
-                     BorderDecoration(
-                         colour = colors[8],
-                         border_width = [0, 0, 2, 0],
-                     )
-                 ],
-                 ),
-        widget.Spacer(length = 8),
-        widget.Systray(padding = 3),
-        widget.Spacer(length = 8),
+   #             widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                   chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                    ),       
 
 
-    ],
+###################### right widgets #############################
+######################## network speed 
+                   widget.Net(
+                   #format="{down} ↓↑ {up}",  # Display format          
+                   # background="#000000",     # Background color        
+                   padding=5,
+                    foreground="#9891ca",
+                     decorations=[
+                          BorderDecoration(
+                               colour = "#9891ca", 
+                               border_width = [0, 0, 3, 0],
+                          )
+                          ], 
+
+                   ),
+
+widget.Spacer(length = 8),
+######################## cpu
+                   widget.CPU(
+                    format = '󰍛  Cpu: {load_percent}%',
+                    foreground="#499dd7",
+                     decorations=[
+                          BorderDecoration(
+                               colour = "#499dd7", 
+                               border_width = [0, 0, 3, 0],
+                          )
+                          ], 
+                   ),
+
+widget.Spacer(length = 8),
+######################## ram memory
+
+widget.Memory(                                                          
+     foreground = "#98be65",                                    
+     format = '{MemUsed: .0f}{mm}',
+    fmt = '💾  Mem: {} used',                                       
+      decorations=[                                                  
+      BorderDecoration(                                          
+        colour = "#98be65",                                    
+        border_width = [0, 0, 3, 0],
+      )
+       ], 
+), 
+
+widget.Spacer(length = 8),
+######################## battery
+#   widget.UPowerWidget(
+widget.Battery(  
+      foreground = "#ecbe7b",    
+   decorations=[                                                  
+      BorderDecoration(                                          
+        colour = "#ecbe7b",                                    
+        border_width = [0, 0, 3, 0],
+      )
+       ], 
+),
+
+widget.Spacer(length = 8),
+######################### volume pulse audio
+
+
+widget.Volume(
+    volume_app="pavucontrol",  # Volume control app
+    volume_up_command="amixer -D pulse sset Master 5%+",  
+    volume_down_command="amixer -D pulse sset Master 5%-", 
+    mute_command="amixer -D pulse sset Master toggle",  
+    fmt="Vol: {}",  # Display format
+foreground = "#e56160",    
+ decorations=[                                                  
+      BorderDecoration(                                          
+        colour = "#e56160",                                    
+        border_width = [0, 0, 3, 0],
+      )
+       ], 
+),
+
+widget.Spacer(length = 8), 
+######################## clock
+     widget.Clock(
+         format="  %Y-%m-%d %a %I:%M %p",
+           foreground = "#499dd7",
+             decorations=[
+                     BorderDecoration(
+                         colour = "#499dd7",
+                         border_width = [0, 0, 2, 0],
+                     )
+                 ],
+     ),
+
+
+widget.Spacer(length = 8),   
+######  widget.QuickExit(),
+
+######################## systray
+                widget.Systray(),
+
+
+
+
+            ],
             size=22,
             background="#1e2030",  # Background color
- #           fontsize = 12,
-  #          font="Ubuntu Bold",
-   #         fontweight="bold",    # Make the font bolder
-    #        opacity=1,  # Transparency
-
-      font="Ubuntu Bold",
-      fontsize = 12,
-      padding = 0,
-   #   background=colors[0]   
-
+            fontsize = 12,
+            font="Ubuntu Bold",
+            fontweight="bold",    # Make the font bolder
+            opacity=1,  # Transparency
+            # padding = 0, 
+            #  margin=[5, 5, 0, 5],
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
 
     ),
@@ -426,20 +377,6 @@ screens = [
 
 
 
-
-
-
-
-
-
-#######################################################3333
-
-#            size=22,
-#          background="#1e2030",  # Background color
-#       fontsize = 12,
-#            font="Ubuntu Bold",
- #           fontweight="bold",    # Make the font bolder
-  #          opacity=1,  # Transparency
 
 
 
